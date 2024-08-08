fe01b1e84c04da307d61c12212cc1156d0f648d7
Henrique0702 <Henriquebd08@gmail.com>
2024-08-08 12:28:38 -0300

    pizza

---
+++ src/shared/Pizzas.jsx
@@ -0,0 +1,45 @@
+
+import PizzaBrocolis from "../assets/imagens/PizzaBrocolis.png"
+import PizzadeCalabresa from "../assets/imagens/PizzadeCalabresa.png"
+import PizzadeCheddareBacon from "../assets/imagens/PizzadeCheddareBacon.png"
+import PizzadeFerreiroRocher from "../assets/imagens/PizzadeFerreiroRocher.png"
+import PizzadeFrango from "../assets/imagens/PizzadeFrango.png"
+import Pizzadenutella from "../assets/imagens/Pizzadenutella.png"
+import Pizzadequatroqueijos from "../assets/imagens/Pizzadequatroqueijos.png"
+import PizzadeStrogonoff from "../assets/imagens/PizzadeStrogonoff.png"
+import PizzaEspanhola from "../assets/imagens/PizzaEspanhola.png"
+import Pizzaimport from "../assets/imagens/Pizzadenutella.png"egana from "../assets/imagens/Pizzadenutella.png"
+
+const Pizzas = [
+{
+"id": 1,
+"nome": "Brocolis",
+"descricao": "Molho de tomate, mussarela, tomate e Brocolis.",
+"imagem": PizzaBrocolis
+},
+{
+"id": 2,
+"nome": "Calabresa",
+"descricao": "Molho de tomate, mussarela e linguiça calabresa.",
+"imagem": PizzadeCalabresa
+},
+{
+"id": 3,
+"nome": "Cheddar e Bacon",
+"descricao": "Molho de tomate, mussarela, Cheggar e Bacon.",
+"imagem": PizzadeCheddareBacon
+},
+{
+"id": 4,
+"nome": "Ferreiro Rocher",
+"descricao": "Nutella, Bom Bom de Ferreiro Rocher e coco ralado.",
+"imagem": PizzadeFerreiroRocher
+},
+{
+"id": 5,
+"nome": "Frango",
+"descricao": "Molho de tomate, mussarela e Frango.",
+"imagem": PizzadeFrango
+},
+]
+export default Pizzas;
