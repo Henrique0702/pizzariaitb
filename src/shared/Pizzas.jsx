bba837e167a1e0f2550f095941c3aaceb5f66eec
Henrique0702 <Henriquebd08@gmail.com>
2024-08-09 11:26:49 -0300

    pizza final

--- src/shared/Pizzas.jsx
+++ src/shared/Pizzas.jsx
@@ -8,38 +8,70 @@ import Pizzadenutella from "../assets/imagens/Pizzadenutella.png"
 import Pizzadequatroqueijos from "../assets/imagens/Pizzadequatroqueijos.png"
 import PizzadeStrogonoff from "../assets/imagens/PizzadeStrogonoff.png"
 import PizzaEspanhola from "../assets/imagens/PizzaEspanhola.png"
-import Pizzaimport from "../assets/imagens/Pizzadenutella.png"egana from "../assets/imagens/Pizzadenutella.png"
+import PizzaVegana from "../assets/imagens/PizzaVegana.png"
 
 const Pizzas = [
 {
 "id": 1,
 "nome": "Brocolis",
-"descricao": "Molho de tomate, mussarela, tomate e Brocolis.",
+"descricao": "Molho de tomate, mussarela, tomate e Brócolis.",
 "imagem": PizzaBrocolis
 },
 {
 "id": 2,
 "nome": "Calabresa",
-"descricao": "Molho de tomate, mussarela e linguiça calabresa.",
+"descricao": "Molho de tomate, mussarela, linguiça calabresa e azeitona.",
 "imagem": PizzadeCalabresa
 },
 {
 "id": 3,
 "nome": "Cheddar e Bacon",
-"descricao": "Molho de tomate, mussarela, Cheggar e Bacon.",
+"descricao": "Molho de tomate, mussarela, Cheggar, Bacon e queijo ralado.",
 "imagem": PizzadeCheddareBacon
 },
 {
 "id": 4,
 "nome": "Ferreiro Rocher",
-"descricao": "Nutella, Bom Bom de Ferreiro Rocher e coco ralado.",
+"descricao": "Chocolate, BomBom de Ferreiro Rocher e coco ralado.",
 "imagem": PizzadeFerreiroRocher
 },
 {
 "id": 5,
 "nome": "Frango",
-"descricao": "Molho de tomate, mussarela e Frango.",
+"descricao": "Molho de tomate, mussarela, Frango e oregano.",
 "imagem": PizzadeFrango
+},
+{
+"id": 6,
+"nome": "Nutella",
+"descricao": "Nutella, granulado e coco ralado.",
+"imagem": Pizzadenutella
+},
+{
+"id": 7,
+"nome": "Quatro queijos",
+"descricao": "Molho de tomate, mussarela, parmesão, gorgonzola e requeijão.",
+"imagem": Pizzadequatroqueijos
+},
+
+{
+"id": 8,
+"nome": "Strogonoff",
+"descricao": "Molho de tomate, mussarela e Strogonoff de carne .",
+"imagem": PizzadeStrogonoff
+},
+
+{
+"id": 9,
+"nome": "Espanhola",
+"descricao": "Molho de tomate, mussarela, calabresa ralada, tomate e oregano.",
+"imagem": PizzaEspanhola
+},
+{
+"id": 10,
+"nome": "Vegana",
+"descricao": "Molho de tomate, manjericão, tomate cereja, azeitonas pretas, milho, ervilha e brócolis .",
+"imagem": PizzaVegana
 },
 ]
 export default Pizzas;
